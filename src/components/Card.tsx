cfcb9baa9a33161992b5a8c36cb7487d4796582c
Arigbede Omobolaji <arigbedeomobolaji@gmail.com>
2023-12-16 19:42:45 +0100

    done with projects

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
-import { Card as AntdCard, Button } from 'antd';
-import Image from 'next/image';
+import Magnet from "@src/assets/svg/magnet.svg"
+import Github from "@src/assets/svg/github.svg"
+import Image, { StaticImageData } from 'next/image';
 import Link from 'next/link';
+import { Url } from 'next/dist/shared/lib/router/router';
 
-export default  function Card() {
+interface CardProps {
+    projectName: String,
+    projectDescription: String,
+    projectImage?: any | String,
+    projectWebsite?: Url;
+    projectRepo: Url;
+    isLive: Boolean
+}
+
+
+export default  function Card({projectName, projectDescription, projectImage, projectWebsite, projectRepo, isLive}: CardProps): React.ReactElement {
     return (
   <div
-        className='flex flex-col  gap-3 max-w-[350px] border-gray-100 rounded-md border'
+        className='flex flex-col  gap-5 max-w-[350px] border-gray-100 rounded-md border'
   >
-    <div className="h-[350px] w-full relative flex items-center justify-center bgGradient">
+    <div className="h-[300px] w-full relative flex items-center justify-center bgGradient hover:scale-110">
         <div className="w-full h-full blur-sm opacity-90">
             <Image
                 priority
@@ -22,9 +34,10 @@ export default  function Card() {
             />
         </div>
         {/* Overlay */}
+        <div className='absolute top-0 rounded-lg left-0 h-full w-full overflow-hidden'>
         <Image
-            className="absolute top-7 rounded-lg left-7 h-10/12 w-10/12 object-contain z-10"
-            src={"https://images.unsplash.com/photo-1633783714421-332b7f929148?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Tm8lMjBpbWFnZXxlbnwwfHwwfHx8MA%3D%3D"}
+            className="h-full w-full object-contain z-10"
+            src={projectImage ? projectImage : "https://images.unsplash.com/photo-1633783714421-332b7f929148?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Tm8lMjBpbWFnZXxlbnwwfHwwfHx8MA%3D%3D"}
             alt="title"
             priority
             width={0}
@@ -32,14 +45,20 @@ export default  function Card() {
             sizes="100vw"
             
         />
+        </div>
+        
      </div>
-    <div className='px-3'>
-        <h1 className='headingText textGradient'>Project Name</h1>
-        <p className='text-gray-300 text-sm  text-justify leading-7'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt quas necessitatibus similique nobis, accusantium quaerat? Perferendis debitis officiis at vel corporis laudantium pariatur accusamus consequuntur velit, reiciendis, laborum voluptate veritatis!</p>
+    <div className='px-3 flex flex-grow flex-col flex-1'>
+        <h1 className='headingText textGradient text-[22px]'>{projectName}</h1>
+        <p className='text-gray-500 text-sm  text-justify leading-6'>{projectDescription.substring(1,200)}..</p>
        
         <div className='flex justify-between items-center p-3'>
-            <Link target='blank' href="#" className={`bg-contentPrimary p-2 px-3 text-gray-900 font-bold flex items-center justify-center rounded-md `}>Demo</Link>
-            <Link target="blank" href="#" className={`bg-contentSecondary text-gray-50 p-2 px-3 font-bold flex items-center justify-center rounded-md `}>Repo</Link>
+           {projectWebsite && <Link target='blank' href={projectWebsite} className={`text-contentPrimary p-2 px-3 border-b border-b-gray-500 flex items-center justify-center rounded-md gap-3`}>
+            <Image src={Magnet} alt="Magnet" />
+            Demo</Link>}
+            <Link target="blank" href={projectRepo} className={`text-contentSecondary p-2 px-3 border-b border-b-gray-500 items-center justify-center rounded-md flex gap-4`}>
+                <Image src={Github} alt="github" />
+                Repo</Link>
         </div>
     </div>
 
--- src/components/MyProjects.tsx
+++ src/components/MyProjects.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import Container from './Container'
+import { projects } from '@src/lib/projects'
 import Card from './Card'
 
 export default function MyProjects() {
   return (
     <div id="projects">
     <Container title="My Projects" description='Projects I have worked on.'>
-        <div>
-            <Card />
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 place-items-start md:place-items-center gap-20'>
+            {
+              projects.map((project) => (
+                <Card key={project.id} {...project} />
+              ))
+            }
         </div>
     </Container>
     </div>
