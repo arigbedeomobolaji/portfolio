57fe52ee06b656cd66fcdcf34396c728ef35df45
Arigbede Omobolaji <arigbedeomobolaji@gmail.com>
2023-12-16 18:34:25 +0100

    created seed data for my projects

---
+++ src/lib/projects.ts
@@ -0,0 +1,85 @@
+import airbnb from "@src/assets/projects/airbnb.png";
+import covid19 from "@src/assets/projects/covid19.png";
+import ecomm from "@src/assets/projects/ecomm.png";
+import gpt from "@src/assets/projects/gpt.png";
+import movieX from "@src/assets/projects/airbnb.png";
+import netflix from "@src/assets/projects/netflix.png";
+import sociopedia from "@src/assets/projects/sociopedia.png";
+import sportsOdds from "@src/assets/projects/sports-odds.png";
+import spotify from "@src/assets/projects/spotify.png";
+
+
+export const projects = [{
+    id: 1,
+    projectImage: airbnb,
+    projectWebsite: "https://airbnb-clone-arigbedeomobolaji.vercel.app/",
+    projectRepo: "https://github.com/arigbedeomobolaji/airbnb_clone.git",
+    projectName: "Airbnb Clone",
+    description: "A sleek and responsive app crafted with React, Tailwind CSS, and Next.js. Its dynamic components, modern design with Tailwind, and efficient routing through Next.js create an engaging user interface. From interactive dashboards to real-time updates, this app embodies a seamless fusion of top-tier technologies for optimal performance and user experience.",
+    isLive: true
+},{
+    id: 2,
+    projectImage: covid19,
+    projectWebsite: "https://omobolaji-covid19-tracker.web.app/",
+    projectRepo: "https://github.com/arigbedeomobolaji/covid-19-tracker.git",
+    projectName: "Covid19 Tracker",
+    description: "A sleek and responsive app crafted with React, Tailwind CSS, and Next.js. Its dynamic components, modern design with Tailwind, and efficient routing through Next.js create an engaging user interface. From interactive dashboards to real-time updates, this app embodies a seamless fusion of top-tier technologies for optimal performance and user experience.",
+    isLive: true
+},{
+    id: 3,
+    projectImage: gpt,
+    projectWebsite: "https://omobolaji-gpt3.vercel.app/",
+    projectRepo: "https://github.com/arigbedeomobolaji/omobolaji_gpt3.git",
+    projectName: "gpt",
+    description: "A sleek and responsive app crafted with React, Tailwind CSS, and Next.js. Its dynamic components, modern design with Tailwind, and efficient routing through Next.js create an engaging user interface. From interactive dashboards to real-time updates, this app embodies a seamless fusion of top-tier technologies for optimal performance and user experience.",
+    isLive: true
+},{
+    id: 4,
+    projectImage: ecomm,
+    projectWebsite: "https://ecomm-omobolaji.vercel.app/",
+    projectRepo: "https://github.com/arigbedeomobolaji/ecomm-omobolaji.git",
+    projectName: "Ecomm",
+    description: "An MERN-based ecommerce website seamlessly integrates MongoDB, Express, React, and Node.js. Enhanced by Amazon S3 for robust storage solutions and Redux for state management, the platform delivers a secure, scalable, and feature-rich shopping experience. This powerful combination ensures efficient data flow, dynamic user interfaces, and streamlined management of product information for an optimal online retail experience.",
+    isLive: true
+},{
+    id: 5,
+    projectImage: movieX,
+    projectWebsite: "https://omobolaji-moviex.vercel.app/",
+    projectRepo: "https://github.com/arigbedeomobolaji/movieX.git",
+    projectName: "MovieX",
+    description: "Explore an advanced movie review app built with MERN stack, Apollo GraphQL, and Apollo Client with Optimistic Response. Utilizing MySQL and Sequelize for robust database management, this platform delivers dynamic user experiences. Share and discover film insights seamlessly, immersing yourself in an innovative movie review community.",
+    isLive: true
+},{
+    id: 6,
+    projectImage: sociopedia,
+    projectWebsite: "https://sociopedia-omobolaji.vercel.app/",
+    projectRepo: "https://github.com/arigbedeomobolaji/sociopedia.git",
+    projectName: "Sociopedia",
+    description: "Embark on a modern social media experience with an app expertly crafted using MERN stack, Next.js, TypeScript, AWS S3, and Redux. This platform seamlessly integrates MongoDB, Express, React, and Node.js for optimal performance. TypeScript elevates code quality, AWS S3 ensures robust storage, and Redux manages state seamlessly, creating a dynamic and efficient user-driven social environment.",
+    isLive: true
+},{
+    id: 7,
+    projectImage: netflix,
+    projectWebsite: "https://netflix-clone-omobolaji.vercel.app/",
+    projectRepo: "https://github.com/arigbedeomobolaji/netflix-clone-omobolaji.git",
+    projectName: "Netflix CLone",
+    description: "Experience the essence of entertainment with a Netflix clone meticulously developed using MERN stack and enriched with Redux for state management. This platform seamlessly integrates MongoDB, Express, React, and Node.js, providing a responsive and immersive streaming experience. Enjoy a user-friendly interface and seamless content navigation on this feature-packed Netflix replica.",
+    isLive: true
+},{
+    id: 8,
+    projectImage: sportsOdds,
+    projectWebsite: "https://sport-odds-client.vercel.app/",
+    projectRepo: "https://github.com/arigbedeomobolaji/sport-odds.git",
+    projectName: "Sport odds",
+    description: "Dive into the world of sports betting with a cutting-edge frontend app developed using Next.js, Tailwind CSS, Redux, and TypeScript. This application seamlessly integrates Next.js for efficient routing, Tailwind for a sleek and responsive design, Redux for state management, and TypeScript for enhanced code quality. Enjoy a dynamic and intuitive betting experience with this powerful and user-friendly sports betting app.",
+    isLive: true
+},{
+    id: 9,
+    projectImage: spotify,
+    projectWebsite: "https://omobolaji-spotify.vercel.app/",
+    projectRepo: "https://github.com/arigbedeomobolaji/spotify_clone.git",
+    projectName: "Spotify Clone",
+    description: "Immerse yourself in the realm of sports wagering through an advanced frontend application created with Next.js, Tailwind CSS, Redux, and TypeScript. This app harmoniously blends Next.js for streamlined routing, Tailwind for a polished and adaptive design, Redux for effective state management, and TypeScript for elevated code quality. Revel in the dynamic and user-friendly betting experience offered by this robust application.",
+    isLive: true
+},
+]
